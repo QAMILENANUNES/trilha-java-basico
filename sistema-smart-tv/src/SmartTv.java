eacba0d8ec07f8bb4fd7bb911b9c60a66dea737d
milena <milenagastro2018@gmail.com>
2022-12-09 17:03:57 -0300

    metodos em prática

---
+++ sistema-smart-tv/src/SmartTv.java
@@ -0,0 +1,24 @@
+public class SmartTv {
+
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void aumentarVolume() {
+        volume++;
+        // volume =volume + 1;
+    }
+
+    public void diminuirVolume() {
+        volume--;
+    }
+
+    public void ligar() {
+        ligada = true;
+    }
+
+    public void desligar() {
+        ligada = false;
+    }
+
+}
